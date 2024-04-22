3afe452d57d8dae2759dd9c988e1c4712b2fe744
Sounak Kundu <sounak.k@sigmoidanalytics.com>
2024-04-22 17:48:02 +0530

    updates

--- snowflake/giveDataNOW.py
+++ snowflake/giveDataNOW.py
@@ -2,8 +2,8 @@ import random
 import datetime
 import requests
 
-# rows = random.randint(10000+1,100000-1)
-rows = 78912
+rows = random.randint(10000+1,100000-1)
+# rows = 10**7 # ~ 989 MB
 
 # Way too dank
 flatten = (lambda f: lambda v: f(f,v,0))(lambda this, l, pos: (this(this, l[pos], 0) + this(this, l, pos+1)) if (type(l)==list and pos < len(l)) else ([l] if type(l)!=list else []))
@@ -35,12 +35,20 @@ pickOne = lambda s: s[random.randint(0,len(s)-1)]
 names = [pickOne(nameSet) for r in range(rows)]
 
 # ID
-ids = ["%04d"%i for i in range(rows)]
+ids = [i for i in range(rows)]
 random.shuffle(ids)
 
 # Phone numbers
 ph_no = lambda prefix: prefix + ''.join(str(random.randint(0,9)) for i in range(10))
-ph_nos = [ph_no("+91") for r in range(rows)]
+
+phoneBook = set()
+_ctr = 0
+while(len(phoneBook)!=rows):
+    phoneBook.add(ph_no("+91"))
+    _ctr += 1
+print("Phone numbers generated per record: ",_ctr/rows)
+
+ph_nos = list(phoneBook)
 
 # Email Ids:
 
@@ -53,7 +61,7 @@ domainEnd = lambda: pickOne(['com','net','org','dev','io','cc','to','me'])
 domain = (lambda f: lambda: f(f))(lambda this: pickOne(nouns) + '.' + (this(this) if random.random()>0.8 else domainEnd()))
 
 # emails = [names[i].lower() + "%03d"%(random.randint(0,rows)%1000) + '@' + domain() for i in range(rows)]
-emails = [names[i].lower() + ids[i] + '@' + domain() for i in range(rows)]
+emails = [names[i].lower() + "%04d"%i + '@' + domain() for i in range(rows)]
 
 # Hire Date:
 # referenceTime = datetime.datetime.fromtimestamp(0)
@@ -73,16 +81,34 @@ yearsHired = [(datetime.datetime.now() - d).days//365 for d in hiredates]
 # Age (18 <= age <= 60)
 ages = [yearsHired[i] + random.randint(lowerLimit, upperLimit - yearsHired[i]) for i in range(rows)]
 
-
 # Salary
-sal_max = 9791835
+sal_max = 76914
 salaries = [random.randint(100,sal_max) for i in range(rows)]
 
+# Manager
+# Q. Are these trees distributed uniformly distributed ? (Assuming randint(a,b) is uniformly distributed)
+# A: No, if a company/organisation is an unordered labelled tree.
+# These trees are uniformly distributed if and only if assumed to be ordered and labelled.
+managers = [-1 for i in range(rows)] # managers[i] = 'id' of manager of i ;
+
+# calcTree():
+stack = [(rows-1,0,rows-1-1)]
+while(len(stack)!=0):
+    rt,l,r = stack.pop()
+
+    if(l>r):
+        continue
+
+    x = random.randint(l,r)
+    managers[x] = ids[rt]
+
+    stack.append((rt, l, x-1)) # Not rooted at ids[x]
+    stack.append((x, x+1, r)) # Rooted at ids[x]
 
 
-with open("empData.csv","w") as f:
-    f.write("id, name, phone, email, hiredate, age, salary\n")
-    for r in zip(ids,names,ph_nos,emails,hiredates,ages,salaries):
+with open("bigEmpData.csv", "w") as f:
+    f.write("id, name, phone, email, hiredate, age, salary, manager\n")
+    for r in sorted(zip(ids,names,ph_nos,emails,hiredates,ages,salaries,managers),key = lambda x: x[0]):
         f.write(', '.join(str(v) for v in r) + '\n')
 
 
